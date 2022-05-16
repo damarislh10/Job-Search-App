b7c22a7df81cc535df63e1a993e9ab75919cb2af
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-05-16 11:19:40 -0500

    changes nav

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -20,6 +20,7 @@ import PersonIcon from "@mui/icons-material/Person";
 export const NavBar = () => {
   const [open, setOpen] = React.useState(false);
   const anchorRef = React.useRef(null);
+  
 
   const handleToggle = () => {
     setOpen((prevOpen) => !prevOpen);
